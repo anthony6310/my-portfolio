45f713fc4881c49b2ee045e1672452508bdb1683
anthony6310 <anthony6310322@yahoo.com>
2023-11-06 22:08:38 -0500

    Links Open In New Tab

--- src/components/ContactMe/ContactMe.jsx
+++ src/components/ContactMe/ContactMe.jsx
@@ -5,9 +5,9 @@ import "./ContactMeStyles.css";
 function ContactMe() {
     return (
         <div className="contact-me">
-            <a href="mailto:anthonypumarwd@gmail.com" className="contact-me__button">Email</a>
-            <a href="https://github.com/anthony6310" className="contact-me__button">GitHub</a>
-            <a href="https://www.linkedin.com/in/anthony-pumar-5a54b4125/" className="contact-me__button">LinkedIn</a>
+            <a href="mailto:anthonypumarwd@gmail.com" target="_blank" className="contact-me__button">Email</a>
+            <a href="https://github.com/anthony6310" target="_blank" className="contact-me__button">GitHub</a>
+            <a href="https://www.linkedin.com/in/anthony-pumar-5a54b4125/" target="_blank" className="contact-me__button">LinkedIn</a>
         </div>
     );
 }
